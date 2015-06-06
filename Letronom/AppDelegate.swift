cd1962b5d7993e3018c112470be4a2fcf8cea397
Shohei Urano <s.runoa@gmail.com>
2015-06-06 15:24:57 +0900

    Initial Commit

---
+++ Letronom/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Letronom
+//
+//  Created by Shohei Urano on 2015/06/06.
+//  Copyright (c) 2015年 Shohei Urano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Letronom/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Letronom
+//
+//  Created by Shohei Urano on 2015/06/06.
+//  Copyright (c) 2015年 Shohei Urano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
