e0abb6a5da9e4720fc7a9a855b0afc5f4577c63a
Shohei Urano <s.runoa@gmail.com>
2015-06-09 19:32:37 +0900

    メニューを追加したりカウント入れたり

--- Letronom/ViewController.swift
+++ Letronom/ViewController.swift
@@ -10,6 +10,8 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    
     func getNextTime () -> NSDate {
         let calendar = NSCalendar.currentCalendar()
         let today = NSDate()
@@ -19,12 +21,6 @@ class ViewController: NSViewController {
         var nextTime: NSDate! = calendar.dateByAddingUnit(NSCalendarUnit.CalendarUnitMinute, value: minuteDiff, toDate: today, options: nil)
         nextTime = calendar.dateByAddingUnit(NSCalendarUnit.CalendarUnitSecond, value: secondDiff, toDate: nextTime, options: nil)
         return nextTime!
-    }
-    
-    func getAdditionalTitle () -> String {
-        var formatter = NSDateFormatter()
-        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
-        return formatter.stringFromDate(NSDate())
     }
     
     func setNotification () {
@@ -40,6 +36,40 @@ class ViewController: NSViewController {
     func setNotifications () {
         self.setNotification()
         NSTimer.scheduledTimerWithTimeInterval(60 * 60, target: self, selector: "setNotification", userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "refreshTimer", userInfo: nil, repeats: true)
+    }
+    
+    func refreshTimer () {
+        let calendar = NSCalendar.currentCalendar()
+        let today = NSDate()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitHour | NSCalendarUnit.CalendarUnitMinute | NSCalendarUnit.CalendarUnitSecond, fromDate: today)
+        var nextHour = components.hour + 1
+        let minuteDiff = 60 - components.minute - 1
+        let secondDiff = 60 - components.second
+        if nextHour > 24 {
+            nextHour = 0
+        }
+        statusItem.title = NSString(format: "%02d,%02d:%02d", nextHour, minuteDiff, secondDiff) as String
+    }
+    
+    func setStatusItem () {
+        let menu = NSMenu()
+        statusItem.title = ""
+        statusItem.highlightMode = true
+        statusItem.menu = menu
+
+        let menuTitle = NSMenuItem()
+        menuTitle.title = "Letronom"
+        menu.addItem(menuTitle)
+
+        let menuQuit = NSMenuItem()
+        menuQuit.title = "Quit"
+        menuQuit.action = Selector("quit:")
+        menu.addItem(menuQuit)
+    }
+    
+    func quit (sender: NSButton) {
+        NSApplication.sharedApplication().terminate(self)
     }
 
     override func viewDidLoad() {
@@ -47,6 +77,7 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
         self.setNotifications()
+        self.setStatusItem()
     }
 
     override var representedObject: AnyObject? {
